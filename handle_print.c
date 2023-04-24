8f0d84d644c13032bdc21149d2bdc688c1c872a8
Bright <brightdjogatse@gmail.com>
2023-04-24 15:51:22 -0700

    printf

--- handle_print.c
+++ handle_print.c
@@ -1,78 +1,49 @@
 #include "main.h"
-
 /**
- * handler- Format controller
- * @str: String format
- * @list: List of arguments
- * Return: Total size of arguments with the total size of the base string
- **/
-int handler(const char *str, va_list list)
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: 1 or 2;
+ */
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
 {
-	int size, i, aux;
-
-	size = 0;
-	for (i = 0; str[i] != 0; i++)
-	{
-		if (str[i] == '%')
-		{
-			aux = percent_handler(str, list, &i);
-			if (aux == -1)
-				return (-1);
-
-			size += aux;
-			continue;
-		}
-
-		_putchar(str[i]);
-		size = size + 1;
-	}
-
-
-	return (size);
-}
-
-/**
- * percent_handler- Controller for percent format
- * @str: String format
- * @list: List of arguments
- * @i: Iteration
- * Return: Size of the numbers of elements printed
- **/
-int percent_handler(const char *str, va_list list, int *i)
-{
-	int size, j, number_formats;
-	format formats[] = {
-		{'s', print_string}, {'c', print_char},
-		{'d', print_integer}, {'i', print_integer},
-		{'b', print_binary}, {'u', print_unsigned},
-		{'o', print_octal}, {'x', print_hexadecimal_low},
-		{'X', print_hexadecimal_upp}, {'p', print_pointer},
-		{'r', print_rev_string}, {'R', print_rot}
+	int i, unknow_len = 0, printed_chars = -1;
+	fmt_t fmt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+		if (fmt[*ind] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
-	*i = *i + 1;
-
-	if (str[*i] == '\0')
-		return (-1);
-
-	if (str[*i] == '%')
+	if (fmt_types[i].fmt == '\0')
 	{
-		_putchar('%');
-		return (1);
-	}
-
-	number_formats = sizeof(formats) / sizeof(formats[0]);
-	for (size = j = 0; j < number_formats; j++)
-	{
-		if (str[*i] == formats[j].type)
+		if (fmt[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
 		{
-			size = formats[j].f(list);
-			return (size);
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
 		}
-
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
 	}
-
-	_putchar('%'), _putchar(str[*i]);
-
-	return (2);
+	return (printed_chars);
 }
