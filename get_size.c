ad0f9b4b071ed83e12d081087e3b327c7ef0b8f3
Bright <brightdjogatse@gmail.com>
2023-04-24 15:46:25 -0700

    Bright

---
+++ get_size.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * get_size - this Calculates the size to cast the argument
+ * @format: Format string in which to print the arguments
+ * @i: List of arguments to be printed.
+ *
+ * Return: Precision.
+ */
+int get_size(const char *format, int *i)
+{
+	int serr_i = *i + 1;
+	int size = 0;
+
+	if (format[serr_i] == 'l')
+		size = S_LONG;
+	else if (format[serr_i] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*i = serr_i - 1;
+	else
+		*i = serr_i;
+
+	return (size);
+}
